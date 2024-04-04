9a85ba451b3e6f6a14f8d09c4a9c694bbf101813
Gustavo Rezin Durigon <gustavo.rezin@gmail.com>
2024-04-04 13:54:17 -0300

    usando react-hook-form

--- src/pages/auth/sign-in.tsx
+++ src/pages/auth/sign-in.tsx
@@ -2,8 +2,27 @@ import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { Helmet } from "react-helmet-async";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+
+const signInForm = z.object({
+  email: z.string().email(),
+});
+
+type SignInForm = z.infer<typeof signInForm>;
 
 export function SignIn() {
+  const {
+    register,
+    handleSubmit,
+    formState: { isSubmitting },
+  } = useForm<SignInForm>();
+
+  async function handleSignIn(data: SignInForm) {
+    await new Promise((resolve) => setTimeout(resolve, 2000));
+    console.log(data);
+  }
+
   return (
     <>
       <Helmet title="Login" />
@@ -18,13 +37,13 @@ export function SignIn() {
             </p>
           </div>
 
-          <form className="space-y-4">
+          <form className="space-y-4" onSubmit={handleSubmit(handleSignIn)}>
             <div className="space-y-2">
               <Label htmlFor="email">Seu e-mail</Label>
-              <Input name="email" type="email" />
+              <Input id="email" type="email" {...register("email")} />
             </div>
 
-            <Button type="submit" className="w-full">
+            <Button type="submit" disabled={isSubmitting} className="w-full">
               Acessar painel
             </Button>
           </form>
