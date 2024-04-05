34c794c4a9a3d24da9f4e899d9359f5f456f9113
Gustavo Rezin Durigon <gustavo.rezin@gmail.com>
2024-04-04 21:15:48 -0300

    página: cadastro de restaurante

--- src/pages/auth/sign-in.tsx
+++ src/pages/auth/sign-in.tsx
@@ -3,6 +3,7 @@ import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { Helmet } from "react-helmet-async";
 import { useForm } from "react-hook-form";
+import { Link } from "react-router-dom";
 import { toast } from "sonner";
 import { z } from "zod";
 
@@ -22,6 +23,7 @@ export function SignIn() {
   async function handleSignIn(data: SignInForm) {
     try {
       await new Promise((resolve) => setTimeout(resolve, 2000));
+      console.log(data);
       toast.success("Enviamos um link de autenticação para seu e-mail.", {
         action: {
           label: "Reenviar",
@@ -39,6 +41,10 @@ export function SignIn() {
     <>
       <Helmet title="Login" />
       <div className="p-8">
+        <Button variant="ghost" asChild className="absolute right-8 top-8">
+          <Link to="/sign-up">Novo estabelecimento</Link>
+        </Button>
+
         <div className="flex w-[350px] flex-col justify-center gap-6">
           <div className="flex flex-col gap-2 text-center">
             <h1 className="text-2xl font-semibold tracking-tight">
@@ -48,7 +54,6 @@ export function SignIn() {
               Acompanhe suas vendas pelo painel do parceiro!
             </p>
           </div>
-
           <form className="space-y-4" onSubmit={handleSubmit(handleSignIn)}>
             <div className="space-y-2">
               <Label htmlFor="email">Seu e-mail</Label>
