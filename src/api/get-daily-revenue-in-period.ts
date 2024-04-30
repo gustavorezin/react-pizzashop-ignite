5b3e2cf65fccf546f7a0fb2c8af87660cc23ec88
Gustavo Rezin Durigon <gustavo.rezin@gmail.com>
2024-04-30 16:02:18 -0300

    receita no período

--- src/api/get-daily-revenue-in-period.ts
+++ src/api/get-daily-revenue-in-period.ts
@@ -1,13 +1,27 @@
 import { api } from "@/lib/axios";
 
+export interface getDailyRevenueInPeriodParams {
+  from?: Date;
+  to?: Date;
+}
+
 export type getDailyRevenueInPeriodResponse = {
   date: string;
   receipt: number;
 }[];
 
-export async function getDailyRevenueInPeriod() {
+export async function getDailyRevenueInPeriod({
+  from,
+  to,
+}: getDailyRevenueInPeriodParams) {
   const response = await api.get<getDailyRevenueInPeriodResponse>(
     "/metrics/daily-receipt-in-period",
+    {
+      params: {
+        from,
+        to,
+      },
+    },
   );
   return response.data;
 }
--- src/pages/app/dashboard/revenue-chart.tsx
+++ src/pages/app/dashboard/revenue-chart.tsx
@@ -6,7 +6,12 @@ import {
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
+import { DateRangePicker } from "@/components/ui/date-range-picker";
+import { Label } from "@/components/ui/label";
 import { useQuery } from "@tanstack/react-query";
+import { subDays } from "date-fns";
+import { useMemo, useState } from "react";
+import { DateRange } from "react-day-picker";
 
 import {
   CartesianGrid,
@@ -20,10 +25,28 @@ import {
 import colors from "tailwindcss/colors";
 
 export function RevenueChart() {
-  const { data: dailyRevenueInPeriod } = useQuery({
-    queryKey: ["metrics", "daily-revenue-in-period"],
-    queryFn: getDailyRevenueInPeriod,
+  const [dateRange, setDateRange] = useState<DateRange | undefined>({
+    from: subDays(new Date(), 7),
+    to: new Date(),
   });
+
+  const { data: dailyRevenueInPeriod } = useQuery({
+    queryKey: ["metrics", "daily-revenue-in-period", dateRange],
+    queryFn: () =>
+      getDailyRevenueInPeriod({
+        from: dateRange?.from,
+        to: dateRange?.to,
+      }),
+  });
+
+  const chartData = useMemo(() => {
+    return dailyRevenueInPeriod?.map((chartItem) => {
+      return {
+        date: chartItem.date,
+        receipt: chartItem.receipt / 100,
+      };
+    });
+  }, [dailyRevenueInPeriod]);
 
   return (
     <Card className="col-span-6">
@@ -34,12 +57,17 @@ export function RevenueChart() {
           </CardTitle>
           <CardDescription>Receita diária no período</CardDescription>
         </div>
+
+        <div className="flex items-center gap-3">
+          <Label>Período</Label>
+          <DateRangePicker date={dateRange} onDateChange={setDateRange} />
+        </div>
       </CardHeader>
 
       <CardContent>
         {dailyRevenueInPeriod && (
           <ResponsiveContainer width="100%" height={240}>
-            <LineChart data={dailyRevenueInPeriod} style={{ fontSize: 12 }}>
+            <LineChart data={chartData} style={{ fontSize: 12 }}>
               <XAxis dataKey="date" axisLine={false} tickLine={false} dy={16} />
 
               <YAxis
